be28372a79009bab610cb3f0adc573daff3e1f53
Tusarsinghnathawat <tusharsingh_22209@aitpune.edu.in>
2025-07-26 19:25:00 +0530

    added playlist model

---
+++ backend/src/models/playlist.models.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required: true
+    },
+    discription: {
+        type: String
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    videos: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+    ]
+},{timestamps: true})
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
